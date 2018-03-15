450dd13e4d090ba9d375203250e50cb308d52d5b
Kristof <tkristof88@gmail.com>
2018-03-15 14:21:33 +0000

    start the songlist container

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,14 @@
 
 var Trackster = {};
+const API_KEY = "1454f041de2b7fc055633de98ee7f704";
 
 $(document).ready(function() {
-$("#searchbutton").click(function() {
-      $(this).slideUp() ;
+  $("#searchbutton").click(function() {
+    Trackster.searchTracksByTitle($("#searchinput").val())
   });
-}); 
+});
+
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -20,5 +23,8 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + '&api_key=' + API_KEY + "&format=json" ,
+    success: console.log()
+  })
 };
